9484c25c64291bd6e12f2e6f0330d432dcff7418
Bekzod <162580380+Spacs21@users.noreply.github.com>
2024-08-25 05:17:03 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,62 @@
+
+let a = 5;
+let perimetr = 4 * a;
+console.log("Task 1:", perimetr);
+
+
+let yuzasiKvadrat = a * a;
+console.log("Task 2:", yuzasiKvadrat);
+
+
+a = 5;
+let b = 10;
+let yuzasiTogriTortburchak = a * b;
+let perimetrTogriTortburchak = 2 * (a + b);
+console.log("Task 3:", yuzasiTogriTortburchak, perimetrTogriTortburchak); 
+
+
+let d = 10;
+let pi = 3.14;
+let uzunligiAylana = pi * d;
+console.log("Task 4:", uzunligiAylana); 
+
+
+a = 5;
+let hajmiKub = a * a * a;
+let sirtKub = 6 * (a * a);
+console.log("Task 5:", hajmiKub, sirtKub);
+
+
+a = 3;
+b = 4;
+let c = 5;
+let hajmiParalelepiped = a * b * c;
+let sirtParalelepiped = 2 * (a * b + b * c + a * c);
+console.log("Task 6:", hajmiParalelepiped, sirtParalelepiped); 
+
+
+let R = 7;
+let uzunligiDoira = 2 * pi * R;
+let yuzasiDoira = pi * (R * R);
+console.log("Task 7:", uzunligiDoira, yuzasiDoira);
+
+
+a = 4;
+b = 8;
+let ortaArifmetik = (a + b) / 2;
+console.log("Task 8:", ortaArifmetik); 
+
+
+a = 4;
+b = 9;
+let ortaGeometrik = Math.sqrt(a * b);
+console.log("Task 9:", ortaGeometrik);
+
+
+a = 3;
+b = 4;
+let yigindisi = a + b;
+let kopaytmasi = a * b;
+let aKvadrati = a * a;
+let bKvadrati = b * b;
+console.log("Task 10:", yigindisi, kopaytmasi, aKvadrati, bKvadrati);
